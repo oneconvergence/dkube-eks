1a7fd95de15c65bca471456046057a29c156f549
maheshtambe <mahesh.tambe@oneconvergence.com>
2021-01-01 16:09:42 +0530

    nfs server being up on gcp instance required for nfs install test

--- rke/gcp/startup.sh
+++ rke/gcp/startup.sh
@@ -6,3 +6,12 @@ sudo apt update
 sudo apt install -y docker-ce-cli=5:19.03.14~3-0~ubuntu-bionic docker-ce=5:19.03.14~3-0~ubuntu-bionic
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
+
+# nsf server setup
+sudo apt install -y nfs-kernel-server
+sudo mkdir -p /mnt/nfs_share
+sudo chown -R nobody:nogroup /mnt/nfs_share/
+sudo chmod 777 /mnt/nfs_share/
+sudo sed -i -e '$a/mnt/nfs_share *(rw,sync,no_subtree_check)' /etc/exports
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
