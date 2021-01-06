55ed87e5703ffb0b919afdf7a09e486743a9a396
maheshtambe <mahesh.tambe@oneconvergence.com>
2021-01-06 13:31:03 +0530

    remove unatrred sscript for eks

--- eks-getting-started/join-worker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#echo "JOINING THE NODES ..."
-#sleep 10
-terraform output kubeconfig > ~/.kube/config
-terraform output config_map_aws_auth > config_map_aws_auth.yaml
-kubectl apply -f config_map_aws_auth.yaml
-#echo "NODES JOINED SUCCESSFULLY :-)"
